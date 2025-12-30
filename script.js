76da0085bad82aa592dcaea5b9c4b2ea42f97ebf
caiom21 <caiomvasconcelos21@gmail.com>
2025-12-30 12:13:14 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-    const btnMenu = document.querySelector('.btn-menu');
-    const menu = document.querySelector('.menu');
-
-    btnMenu.addEventListener('click', () => {
-      menu.classList.toggle('menu-aberto');
-    }); 
